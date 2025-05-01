bb206c2fe1f6e5fd4565c9ab4a8955cffb1f5180
GellertAnastasia <st130144@student.spbu.ru>
2025-05-01 14:44:46 +0300

    astyle

--- ascii85/ascii85.cpp
+++ ascii85/ascii85.cpp
@@ -1,50 +1,47 @@
 #include "ascii85.h"
 
-
 void encode()
+{
+    const size_t SIZE = 4;
+    char buffer[SIZE];
+    while (true)
     {
-        const size_t SIZE = 4;
-        char buffer[SIZE];
-        while (true) 
+        std::memset(buffer, 0, SIZE);
+        std::cin.read(buffer, SIZE);
+        size_t read = std::cin.gcount();
+        if (read == 0)
         {
-            std::memset(buffer, 0 , SIZE);
-            std::cin.read(buffer, SIZE);
-            size_t read = std::cin.gcount();
-            
-            if (read == 0)
-            {
-                break;
-            }
-            size_t padding = 4 - read;
-              
-            if (padding != 0)
-            {
-                std::memset(buffer + read, 0, padding);
-            }
-            
-            std::cout.write(buffer, SIZE);
+            break;
         }
+        size_t padding = 4 - read;
+        if (padding != 0)
+        {
+            std::memset(buffer + read, 0, padding);
+        }
+        std::cout.write(buffer, SIZE);
     }
+}
 
-void decode() {
+void decode()
+{
     const size_t SIZE = 5;
     char buffer[SIZE];
 
-    while (true) 
+    while (true)
     {
         std::cin.read(buffer, SIZE);
         size_t read = std::cin.gcount();
 
         if (read == 0)
-            {
-                break;
-            }
+        {
+            break;
+        }
 
-        if (read < SIZE) 
+        if (read < SIZE)
         {
             throw std::runtime_error("decoding error");
         }
-        
+
         std::cout.write(buffer, SIZE);
     }
 }
--- ascii85/test.cpp
+++ ascii85/test.cpp
@@ -1,22 +1,29 @@
 #include "ascii85.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     if (argc == 1)
     {
         encode();
     }
-    for (int i = 1; i < argc; ++i) {
+    for (int i = 1; i < argc; ++i)
+    {
         std::string arg = argv[i];
-        if (arg == "-d") {
+        if (arg == "-d")
+        {
             decode();
-        } else if (arg == "-e") {
+        }
+        else if (arg == "-e")
+        {
             encode();
-        } else {
+        }
+        else
+        {
             return 1;
         }
     }
-       
-            
-            
-    return 0;       
+
+
+
+    return 0;
 }
